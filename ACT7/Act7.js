a1aab0d7ddbe9a344d0e3d3216a92c29cbdbb423
eniemed <115500235+eniemed@users.noreply.github.com>
2023-10-02 21:04:04 +0200

    Add files via upload

---
+++ ACT7/Act7.js
@@ -0,0 +1,10 @@
+var entradaNumero = prompt("Introduce un número entero: ")
+var numero = parseInt(entradaNumero);
+
+if (numero % 2 === 0) {
+    console.log("El número introducido es par");
+} else {
+    console.log("El número introducido es impar");
+}
+
+
