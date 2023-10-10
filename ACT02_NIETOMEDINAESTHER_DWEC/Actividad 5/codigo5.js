5be2ffaded68d250305c87e33e12894c2f2a2b55
eniemed <115500235+eniemed@users.noreply.github.com>
2023-10-10 10:33:13 +0200

    Add files via upload

---
+++ ACT02_NIETOMEDINAESTHER_DWEC/Actividad 5/codigo5.js
@@ -0,0 +1,22 @@
+function recetas() {
+    let recetas = [
+        {id: 1, titulo: "Pollo con patatas", explicacion: "Cortar las patatas, freirlas y echarles sal. Por otro lado cortamos el pollo en filetes, especiamos y freimos en una sartén"},
+        {id: 2, titulo: "Macarrones con tomate", explicacion: "Hervir la pasta. Sofreir el tomate con cebolla y ajo. Echar la pasta junto con el tomate y mezclar bien, echar sal si es necesario."},
+        {id: 3, titulo: "Ensalada", explicacion: "Cortamos la lechuga al tamaño deseado. Mezclamos en un bol con maíz, atún, cebolla y huevo duro. Por último echamos sal, vinagre y aceite al gusto."},
+        {id: 4, titulo: "Macedonia", explicacion: "Cortamos a taquitos la variedad de fruta que más nos guste y servimos bien fresca en un bol"}
+    ]
+
+    const lista = document.getElementById("recetas")
+    lista.innerHTML = " "
+    
+    recetas.forEach(element => {
+        
+        let li = document.createElement("li");
+        li.textContent = `${element.id}. ${element.titulo}: ${element.explicacion}`;
+        lista.appendChild(li);
+        
+    });
+
+    
+}
+    
