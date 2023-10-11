3bbd92a51afa6ec4a00f5ac361c756de461f8330
eniemed <115500235+eniemed@users.noreply.github.com>
2023-10-11 13:58:52 +0200

    Add files via upload

---
+++ ACT03_NIETOMEDINAESTHER_DWEC/Actividad 2/codigo2.js
@@ -0,0 +1,25 @@
+/*
+Escribir un script que simule el lanzamiento de 2 dados:
+Utiliza la función Math.random() para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados
+Luego, suma el valor de los 2 dados y almacena su resultado
+Ahora, haz 36.000 lanzamientos
+Por último, muestra por pantalla el número de veces que ha salido cada resultado
+
+*/
+
+
+
+function dados() {
+
+    for (let i = 36000; i >= 0; i--) {
+
+    let dado1 = Math.ceil(Math.random() * 6)
+    let dado2 = Math.ceil(Math.random() * 6)
+
+    let sumaDados = dado1 + dado2
+
+    let mapaResultados = Map
+    
+    }
+}
+
